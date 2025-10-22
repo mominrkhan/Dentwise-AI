48df25301a66bcfd7449f659db37a95601febc6e
mominrkhan <vadehqs@gmail.com>
2025-10-21 22:29:34 -0400

    admin-page implementation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,11 @@
+import Navbar from "@/components/Navbar";
+
 function DashboardPage() {
-    return <div>DashboardPage</div>;
-  }
-  export default DashboardPage;
+  return (
+    <div>
+      <Navbar />
+      DASHBOARD
+    </div>
+  );
+}
+export default DashboardPage;
